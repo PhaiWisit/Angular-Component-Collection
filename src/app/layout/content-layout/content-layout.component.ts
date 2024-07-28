e93baea24fe18125610c0c85f05a7040b1666e81
PhaiWisit <w.phai@hotmail.com>
2024-07-28 18:04:15 +0700

    first commit

---
+++ src/app/layout/content-layout/content-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-layout.component.html',
+  styleUrl: './content-layout.component.css'
+})
+export class ContentLayoutComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+}
